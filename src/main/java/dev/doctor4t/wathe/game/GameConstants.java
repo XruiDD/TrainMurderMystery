49090973ea9369db448a0ec1dd40cedefd362fd2
XruiDD <48876676+XruiDD@users.noreply.github.com>
2026-01-25 01:06:36 +0800

    fix: 游戏时间调整

--- src/main/java/dev/doctor4t/wathe/game/GameConstants.java
+++ src/main/java/dev/doctor4t/wathe/game/GameConstants.java
@@ -74,7 +74,7 @@ public interface GameConstants {
     int FIRECRACKER_TIMER = getInTicks(0, 15);
     int BLACKOUT_MIN_DURATION = getInTicks(0, 30);
     int BLACKOUT_MAX_DURATION = getInTicks(0, 40);
-    int TIME_ON_CIVILIAN_KILL = getInTicks(1, 0);
+    int TIME_ON_CIVILIAN_KILL = getInTicks(0, 30);
 
     static int getInTicks(int minutes, int seconds) {
         return (minutes * 60 + seconds) * 20;
--- src/main/java/dev/doctor4t/wathe/game/gamemode/MurderGameMode.java
+++ src/main/java/dev/doctor4t/wathe/game/gamemode/MurderGameMode.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 
 public class MurderGameMode extends GameMode {
     public MurderGameMode(Identifier identifier) {
-        super(identifier, 10, 6);
+        super(identifier, 15, 6);
     }
 
     private static int assignRolesAndGetKillerCount(@NotNull ServerWorld world, @NotNull List<ServerPlayerEntity> players, GameWorldComponent gameComponent) {
